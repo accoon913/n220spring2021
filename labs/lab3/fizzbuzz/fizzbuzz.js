31aecc19e0366fb2c0fd498c929b6b43d1d61845
Austin Coon <austincoon@outlook.com>
2021-02-11 13:11:11 -0500

    created fizzbuzz

---
+++ labs/lab3/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(1500, 500);
+}
+
+function draw() {
+    background(200);
+    var y = 200;
+    
+    for (i = 0; i < 25; ++ i) { // Loop repeats 25 times
+        
+        var x = i*50; 
+        
+        if (i % 3 == 0 && i % 5 == 0) {
+            fill (25, 106, 227); // Blue
+            square(x, y, 50);
+        }
+        else if (i % 5 == 0) {
+            fill (46, 191, 119); // Green
+            square(x, y, 50);
+        }
+        else if (i % 3 == 0) {
+            fill (185, 59, 227); // Purple
+            circle(x+25, y+25, 25);
+        }
+        else {
+            fill(255);
+            circle(x+25, y+25, 25);
+        }
+    }
+}

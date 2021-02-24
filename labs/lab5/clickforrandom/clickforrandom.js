37df7c88ae4dce926081acfba7b2ec0dc52ba950
Austin Coon <austincoon@outlook.com>
2021-02-24 16:27:01 -0500

    created and started working on click for random

---
+++ labs/lab5/clickforrandom/clickforrandom.js
@@ -0,0 +1,12 @@
+let colors = ["fb743e", "383e56", "9fb8ad", "c5d7bd"]
+
+function setup () {
+    createCanvas(500, 500);
+    background("#c4c4c4");
+}
+
+function draw () {
+    circle(250, 250, 100)
+}
+
+

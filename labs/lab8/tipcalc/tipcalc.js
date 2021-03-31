4764667975cd816c4b5e965f640dbe0c7b277b39
Austin Coon <austincoon@outlook.com>
2021-03-31 12:34:47 -0400

    Started and finished tip calculator

---
+++ labs/lab8/tipcalc/tipcalc.js
@@ -0,0 +1,9 @@
+let txtAmount = document.getElementById("txtAmount")
+
+function calcTip() {
+    let initialAmount = Number(txtAmount.value);
+    let tipAmount = initialAmount * 0.2;
+    let totalAmount = initialAmount + tipAmount;
+
+    console.log("Tip: $", tipAmount, "Total: $", totalAmount);
+}

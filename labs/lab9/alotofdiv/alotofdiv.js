e88ff310e6ec5ef9799ffae3a5b4c62a5eade04f
Austin Coon <austincoon@outlook.com>
2021-04-08 15:29:17 -0400

    Finished the that's a lot of div exercise

--- labs/lab9/alotofdiv/alotofdiv.js
+++ labs/lab9/alotofdiv/alotofdiv.js
@@ -1,8 +1,29 @@
-let dvSquare = document.getElementById("dvSquare");
-
-let h = 20;
-let w = 20;
-
-for (i = 0; i < 100; i++) {
+for (var i = 0; i < 100; i++) {
+    let dvSquare = document.getElementById("dvSquare");
+    // Creating a new div during each loop iteration
+    let dvNew = document.createElement('div');
     
+    // Styling the created div
+    dvNew.style.float = "left";
+    dvNew.style.height = "20px";
+    dvNew.style.width = "20px";
+    // Background-Color is determined by the randomColor function
+    dvNew.style.backgroundColor = randomColor();
+ 
+    // Adds the new div to the HTML
+    dvSquare.appendChild(dvNew); 
+}
+
+
+function randomColor() {
+    // determines the red value
+    var r = Math.round( Math.random() * 255);
+    // determines the green value
+    var g = Math.round( Math.random() * 255);
+    // determines the blue values
+    var b = Math.round( Math.random() * 255);
+    // Adds the variable values together in the rgb format
+    var squareColor = "rgb(" + r + "," + g + "," + b + ")";
+    // Function returns the variables in the rgb format to the backgroundColor style
+    return squareColor;
 }

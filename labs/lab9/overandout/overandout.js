7bb8a0f1657f4baab7a7d3ad23e356428d6c7965
Austin Coon <austincoon@outlook.com>
2021-04-08 15:41:39 -0400

    finished the over and out exercise

--- labs/lab9/overandout/overandout.js
+++ labs/lab9/overandout/overandout.js
@@ -1,1 +1,17 @@
-s
+let dvBox = document.getElementById("dvBox");
+
+dvBox.style.width = "100px";
+dvBox.style.height = "100px";
+dvBox.style.backgroundColor = "#2881ed";
+
+function mouseOver() {
+    dvBox.style.width = "100px";
+    dvBox.style.height = "100px";
+    dvBox.style.backgroundColor = "#000000";
+}
+
+function mouseOut() {
+    dvBox.style.width = "100px";
+    dvBox.style.height = "100px";
+    dvBox.style.backgroundColor = "#2881ed";
+}

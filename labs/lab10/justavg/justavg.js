f181cc6f112dd0ea867a6734622209b1fa105e9a
Austin Coon <austincoon@outlook.com>
2021-04-14 15:57:54 -0400

    finished the its just average exercise

---
+++ labs/lab10/justavg/justavg.js
@@ -0,0 +1,24 @@
+let txtNumbers = document.getElementById("txtNumbers");
+let dvSumResults = document.getElementById("dvSumResults");
+let dvAvgResults = document.getElementById("dvAvgResults");
+
+function doCalcs() {
+    // Variable for my input string
+    let numbString = txtNumbers.value;
+    // Splits the input string on commas
+    let numbArray = numbString.split(",");
+
+    // Used for calculations/ reduce array
+    let sum = 0;
+
+    for(let i = 0; i < numbArray.length; i++) {
+        // Number turns the string values into numbers for calculations
+        sum += Number(numbArray[i]);
+        avg = sum / Number(numbArray.length);
+    }
+    dvSumResults.innerHTML = sum;
+    dvAvgResults.innerHTML = avg;
+    
+    console.log(sum);
+    console.log(avg);
+}
